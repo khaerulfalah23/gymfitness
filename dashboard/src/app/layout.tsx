24898f51c87ff2b1bc16b1b227942036c1f8fe2e
khaerulfalah23 <khaerulfalah23@gmail.com>
2025-01-04 07:16:38 +0700

    feat: remove config react query & axios

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { ReactQueryProvider } from '@/components/providers/ReactQueryProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        {children}
       </body>
     </html>
   );
